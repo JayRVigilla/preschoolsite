2a93ef4bee6a9113761fb6eafeb8da4a7acb0499
Jay Vigilla <JayRVigilla@gmail.com>
2020-07-25 00:27:28 -0700

    CSS and style
    left clues for reactstrap components to use
    added images to Enrollment
    formatted navbar

--- src/Enrollment/index.js
+++ src/Enrollment/index.js
@@ -4,9 +4,18 @@ import './Enrollment.css';
 function Enrollment() {
   return (
     <div className="Enrollment">
-      <h1>Enrollment & Tuition</h1>
-      <h2>We offer tours by appointment.</h2>
-      
+      <div className="lead-in">
+        <img src="https://images.squarespace-cdn.com/content/v1/5228b942e4b041a33a9eaf70/1385324765733-1URVPGFA35SVS0YLMA7Q/ke17ZwdGBToddI8pDm48kJgpvGNLUyfy0ZjJA6GCrRh7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0p4XabXLlNWpcJMv7FrN_NKu5IEN9y7svcBQsXRcfBMyN-kUyIPFgRhKcvzN8HARqA/_MG_2055.JPG?format=300w" alt="two children playing with magnetiles" />
+        <div className="lead-in-text">
+          <h1>Enrollment & Tuition</h1>
+          <h2>We offer tours by appointment.</h2>
+          <p>Our Enrollment is ongoing, when space in our current program allows. For Fall admission  we will be accepting applications starting January of that calendar year. Applications received after this date will be considered on a first come, first serve basis.</p>
+        </div>
+      </div>
+      <p>Please contact us through our website if you have any questions about our program or if you'd like to set up a tour. </p>
+      <p>Once a child is enrolled, parents are encouraged to make arrangements to visit school with their child to acquaint him or her with the staff, environment and classmates prior to their first day of school. It is important for both the parent and the child to feel comfortable in the school.</p>
+      <p>It is required by law, that the school be equipped with a Certificate of Immunization (or signed exemption form), DSS forms, and a copy of the child's health form/physical signed by a doctor. All forms must be submitted before the first day of school or children will not be admitted in their classroom. </p>
+      <img src="https://images.squarespace-cdn.com/content/v1/5228b942e4b041a33a9eaf70/1385325335556-UNV8SUEU9N57UTYCQZEL/ke17ZwdGBToddI8pDm48kNmeqSFDz_zeo8vO5oVbLf97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0jG2lbcDYBOeMi4OFSYem8CUBct1RSDtm3ZBkuEyTmqb5HjcPjL49rgm5oKO6qQ9iw/_MG_1909.JPG?format=750w" alt="Children decorating dreidels for Chanukah." />
     </div>
   );
 }
